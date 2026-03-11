1939425615fddcb6d5c8aba2b19ae4e1d1c48f6c
JyothiYellam <jyothiym23@gmail.com>
2026-03-11 20:40:05 +0530

    conversation completed

--- 05.variables.sh
+++ 05.variables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "please enter your username::"
 read USER_NAME
-echo "username is $USER_NAME"
+echo "username is $USER_NAME" 
+
+echo "please enter your password::"
+read -s PASSWORD
