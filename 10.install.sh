8c8ec0e043711debd94946038b7be5d7b2fa450d
JyothiYellam <jyothiym23@gmail.com>
2026-03-13 10:36:40 +0530

    actions

--- 10.install.sh
+++ 10.install.sh
@@ -6,6 +6,7 @@ if [ $USERID -ne 0 ]; then
  echo "please run this script with root user access"
  exit 1
 fi
+
 echo "Installing Nginx"
 dnf install nginx -y
 
@@ -15,3 +16,24 @@ if [ $? -ne 0 ]; then
 else
  echo "installing nginx ...success"
 fi
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]; then
+ echo "Installing mysql....failure"
+ exit 1
+else
+ echo "installing mysql ...success"
+fi
+
+dnf install nodejs -y 
+
+if [ $? -ne 0 ]; then
+ echo "Installing nodejs....failure"
+ exit 1
+else
+ echo "installing nodejs ...success"
+fi
+
+
+
